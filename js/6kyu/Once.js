33cc15a1e228800f515459859890cf0db415ce36
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-08-04 13:04:56 +0200

    feat: add solution for kata Once

---
+++ js/6kyu/Once.js
@@ -0,0 +1,19 @@
+/*
+You'll implement once, a function that takes another function as an argument, and returns a new version of that function that can only be called once.
+
+Subsequent calls to the resulting function should have no effect (and should return undefined).
+
+For example:
+
+logOnce = once(console.log)
+logOnce("foo") // -> "foo"
+logOnce("bar") // -> no effect
+
+*/
+
+const once = (fn, c = true) => (...args) => {
+    if (c) {
+      c = false;
+      return fn(...args);
+    }
+}
